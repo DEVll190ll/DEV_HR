3e148958aa2febccb37c0e8a88f12ff60a931c2d
DEVll190ll <xasw564321@gmail.com>
2017-07-16 04:16:41 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,54 @@
+do local _ = {
+  about_text = "🚏- اهلا بك عزيزي WeLcOmE\n سورس دراغون   ( Dragon )\n\n〰 ➗ 〰 ✖️ 〰 ➕ 〰\nDeveloper ⛳️🏒  :\n🔸 - @Aaa1R\nChannel sors 🏈  :\n🔹 - @Ch_Dev \n〰 ➗ 〰 ✖️ 〰 ➕ 〰\n\n🛰 - رابط السورس :\nhttps://github.com/Ali-Dragon/Dragon.git : link in githup",
+  enabled_plugins = {
+    "@LL190LL",
+    "DEV_10",
+    "DEV_19",
+    "DEV_2",
+    "addreplay",
+    "DEV_3",
+    "delete",
+    "111",
+    "DEV",
+    "DEV_9",
+    "help",
+    "image",
+    "DEV_16",
+    "DEV_20",
+    "HUSSIN_MOHAMED",
+    "hussin",
+    "DEV_15",
+    "DEV_4",
+    "DEV_6",
+    "DEV_1",
+    "DEV_5",
+    "DEV_7",
+    "DEV_8",
+    "DEV_H",
+    "DEV_11",
+    "DEV_12",
+    "DEV_13",
+    "DEV_14",
+    "DEV_17",
+    "DEV_18",
+    
+    
+    
+
+    
+  
+    
+    
+ },
+  help_text = "[[ْDEV @LL190LL]],",
+  help_text_realm = "[[ْ#BOT_DEV @MX313_BOT]],",
+  help_text_super = "[[ْ#DEV @DEV_HR]]",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    295669242
+  }
+}
+return _
+end
